12bf107e0363814dcd35003d7db40d0375a24fa1
Idris Muazu Yakub <imuazyakub@gmail.com>
2023-10-05 22:27:11 +0100

    annotated function created

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+'''
+annotated module
+'''
+
+def concat(str1: str, str2: str) -> str:
+    ''' concat function that conatnate str1 and str2'''
+    return str(str1 + ' '+str2)
