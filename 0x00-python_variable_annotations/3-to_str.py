61223745307829c47c43d4b71049c147276d5201
Idris Muazu Yakub <imuazyakub@gmail.com>
2023-10-06 00:03:23 +0100

    annotated to_str function created

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+'''
+annotated functions module
+'''
+
+
+def to_str(n: float) -> str:
+    '''function that takes float n and returns its string representation'''
+    return str(n)
