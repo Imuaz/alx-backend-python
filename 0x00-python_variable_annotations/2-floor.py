01f814cb1f6fcd5e06ee82b2d808dc508721799c
Idris Muazu Yakub <imuazyakub@gmail.com>
2023-10-05 23:55:38 +0100

    annotated floor function created

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+''''
+annotated functions module
+'''
+import math
+
+
+def floor(n: float) -> int:
+    ''' floor function that takes a float n and returs its floor'''
+    return math.floor(n)
