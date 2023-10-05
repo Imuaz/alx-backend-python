80f71fb839c047d43b9ffa473d02708737305eef
Idris Muazu Yakub <imuazyakub@gmail.com>
2023-10-06 00:58:06 +0100

    annotated function created

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'''
+annotated functions/variables module
+'''
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    '''function that return the sum of a list of float numbers'''
+    return sum(input_list)
