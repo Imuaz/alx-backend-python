734df661b4912bc348b60d37f16729fe1472ddcf
Idris Muazu Yakub <imuazyakub@gmail.com>
2023-10-06 01:54:26 +0100

    annotated function created

---
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'''
+annotated module
+'''
+from typing import *
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    '''annotated function'''
+    return [(i, len(i)) for i in lst]
