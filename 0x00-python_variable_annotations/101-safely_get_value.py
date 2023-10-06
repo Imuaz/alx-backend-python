8bd92a2f8de111319be0184459e1a7196960a48f
Idris Muazu Yakub <imuazyakub@gmail.com>
2023-10-06 02:36:47 +0100

    advanced

---
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+'''
+annotated module
+'''
+import typing
+
+
+T = typing.TypeVar('T')
+
+
+def safely_get_value(dct: typing.Mapping, key: typing.Any, default: typing
+                     .Union[T, None] = None) -> typing.Union[T, typing.Any]:
+    ''''annotated function'''
+    if key in dct:
+        return dct[key]
+    else:
+        return default
