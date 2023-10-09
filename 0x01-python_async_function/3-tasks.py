1f85524e44ee3f45368b03a181fd6ada4f8a71b4
Idris Muazu Yakub <imuazyakub@gmail.com>
2023-10-10 00:14:31 +0100

    measure the run time

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+'''
+task 3 module
+'''
+import asyncio
+import random
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    ''' function that returns asyncio.Task'''
+    return asyncio.create_task(wait_random(max_delay))
