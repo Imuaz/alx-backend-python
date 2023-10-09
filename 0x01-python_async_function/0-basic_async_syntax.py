9e3b6c91b9e7217586b98462ffc9a7902194ff8f
Idris Muazu Yakub <imuazyakub@gmail.com>
2023-10-09 21:30:17 +0100

    the basic of async

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+'''
+task 0 module
+'''
+import asyncio
+import random
+
+
+async def wait_random(max_delay: int =10):
+    '''function that takes returns a random delay b/w in argument'''
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+
+    return delay
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Backend-Python
 
-
 - [0x00. Python - Variable Annotations](./0x00-python_variable_annotations)
+- [0x01. Python - Async](./0x01-python_async_function)
