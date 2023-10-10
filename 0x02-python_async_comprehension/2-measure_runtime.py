93f59a9e7fbde2ea5cad21f2bebc283fe253da0e
Idris Muazu Yakub <imuazyakub@gmail.com>
2023-10-10 18:54:17 +0100

    coroutine that measure the total runtime created

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+'''
+Measure run time module
+'''
+import asyncio
+from typing import List
+import time
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    ''' function measures the total runtime and return it.'''
+    tasks = [async_comprehension() for _ in range(4)]
+
+    start_time = time.time()
+
+    await asyncio.gather(*tasks)
+
+    end_time = time.time()
+
+    total_runtime = end_time - start_time
+
+    return total_runtime
