ee46cb13ebbae0593fbe046fd9b9058fcbaa0028
Idris Muazu Yakub <imuazyakub@gmail.com>
2023-10-30 17:15:20 +0100

    task1 method implemented

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -14,9 +14,18 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2),
-        ])
+    ])
 
     def test_access_nested_map(self, nested_map, path, expected_result):
         '''test wheather the method return as expected'''
         result = access_nested_map(nested_map, path)
         self.assertEqual(result, expected_result)
+
+    @parameterized.expand([
+        ({}, ("a",), KeyError),
+        ({"a": 1}, ("a", "b"), KeyError)
+        ])
+    def test_access_nested_map_exception(self, nested_map, path, exception_type):
+        '''test for the expected exception raised.'''
+        with self.assertRaises(exception_type):
+            access_nested_map(nested_map, path)
